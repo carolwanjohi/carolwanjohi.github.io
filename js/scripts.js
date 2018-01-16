62803524fc3267e1ccf5b8700ca49152a12eb9be
Carol Wanjohi <wanjohi.carol54@gmail.com>
2018-01-16 21:46:52 +0300

    Smooth scroll * Create scripts.js with smoth scroll functionality

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+    // Smooth Scroll
+    $('a[href^="#"]').on('click',function (event) {
+
+        event.preventDefault();
+
+        var target = this.hash;
+
+        var $target = $(target);
+
+        $('html, body').stop().animate({
+
+            'scrollTop': $target.offset().top
+
+        }, 900, 'swing', function () {
+
+            window.location.hash = target;
+
+        });
+        
+    });
+    
+});
